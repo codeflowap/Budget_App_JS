bd83abbdf8e7d950c84e13bc77c8f3082c70306e
John MOHADDES <johnmohaddes@gmail.com>
2017-08-02 13:25:21 +0430

    minor modifications

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ var UIController = (function() {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expenseContainer: '.expense__list'
+        expenseContainer: '.expenses__list'
         
     };
     
@@ -95,8 +95,7 @@ var UIController = (function() {
             } else if (type === 'exp') {
                 element = DOMstrings.expenseContainer;
                 
-                html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">>%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div>  <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
-                
+                html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div>  <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';  
             }
                   
             
